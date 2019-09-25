77103552ed813f9bc8001d8adcb1b8e756c1bb84
Tetrabyte <dustin@tetra.dev>
2019-09-25 14:40:03 -0700

    Adding upload by url on clipboard

---
+++ clipboard.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source ~/.secrets
+
+MAIN_FOLDER="$HOME/Pictures/Screenshots"
+
+FULL_DAY=$(date +'%A')
+FULL_MONTH=$(date +'%B')
+FULL_YEAR=$(date +'%Y')
+
+HOUR=$(date +'%H')
+MINUTE=$(date +'%M')
+SECOND=$(date +'%S')
+
+DAY=$(date +'%d')
+MONTH=$(date +'%m')
+YEAR=$(date +'%y')
+
+FILE_NAME="${HOUR}-${MINUTE}-${SECOND}-${MONTH}-${DAY}-${YEAR}"
+FOLDER_NAME="$FULL_YEAR/$FULL_MONTH/$FULL_DAY"
+
+# Pull clipboard into variable
+CLIPBOARD=$(pbpaste)
+
+# Make folder(s)
+mkdir -p ${MAIN_FOLDER}/${FOLDER_NAME}
+
+# Download image from link
+curl $CLIPBOARD -s -L > ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.png
+
+# Send Uploading Notification
+osascript -e 'display notification "'"$FILE_NAME"'" with title "Uploading Link from clipboard"'
+
+# Sleep for 1 seconds
+sleep 1
+
+# Upload Screenshot
+FILE_URL=$(curl -F "file=@${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.png" $UPLOADER_URL -H "token: $UPLOADER_TOKEN" -s)
+
+# Copy URL to clipboard
+echo $FILE_URL | pbcopy
+
+# Send Upload Notification
+osascript -e 'display notification "'"$FILE_URL"'" with title "Uploaded link from clipboard"'
