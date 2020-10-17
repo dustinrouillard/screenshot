81c790da19f6cfd6fd0507b51e8678261e907bb3
Dustin Rouillard <me@dustin.sh>
2020-10-16 17:54:29 -0700

    changing things and stuff

--- upload-api.sh
+++ upload-api.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 source ~/.secrets
 source ~/.jwt
 
-MAIN_FOLDER="$HOME/Pictures/Screenshots"
+MAIN_FOLDER="$HOME/Pictures/Uploads"
 
 FULL_DAY=$(date +'%A')
 FULL_MONTH=$(date +'%B')
@@ -19,31 +19,29 @@ YEAR=$(date +'%y')
 
 FILE_NAME="${HOUR}-${MINUTE}-${SECOND}-${MONTH}-${DAY}-${YEAR}"
 FOLDER_NAME="$FULL_YEAR/$FULL_MONTH/$FULL_DAY"
+FILE_EXT=${1##*.}
 
 # Make folder(s)
 mkdir -p ${MAIN_FOLDER}/${FOLDER_NAME}
 
-# Take Screenshot
-FLAGS="-xsCm"
-if [ "$1" != "" ]; then; FLAGS="-xsCmw"; fi
-screencapture ${FLAGS} ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.png
-
-if [ ! -e ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.png ]; then exit 0; fi
+# Move copy item
+cp $1 ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.${FILE_EXT}
 
 # Send Uploading Notification
-osascript -e 'display notification "'"$FILE_NAME"'" with title "Uploading Screenshot"'
+osascript -e 'display notification "'"$FILE_NAME"'" with title "Uploading file"'
 
 # Define file variables for name and base64
-BASE64=$(cat ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.png | base64)
+BASE64=$(cat ${MAIN_FOLDER}/${FOLDER_NAME}/${FILE_NAME}.${FILE_EXT} | base64)
+SHA256=$(echo $BASE64 | openssl sha256)
 
 # Upload file to custom api
-UPLOAD=$(http --ignore-stdin POST ${PERSONAL_API_HOST}/upload/image authorization:$(jwt dustin.sh/api ${PERSONAL_API_INTERNAL_SECRET} 60) file=\;base64,${BASE64})
+UPLOAD=$(http --ignore-stdin POST ${PERSONAL_API_HOST}/upload/file authorization:$(jwt dustin.sh/api ${PERSONAL_API_INTERNAL_SECRET} 20) file=\;base64,${BASE64})
 
 # Get file url
 FILE_URL=$(echo $UPLOAD | jq .data | cut -d "\"" -f 2)
 
 # Copy URL to clipboard
-echo $FILE_URL | pbcopy
+echo $FILE_URL | pbcopy 
 
 # Send Upload Notification
 osascript -e 'display notification "'"$FILE_URL"'" with title "Uploaded Screenshot"'
